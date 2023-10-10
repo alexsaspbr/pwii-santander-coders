c71d904f43666be7a549093fe44307e5f9f19dd3
Alex Araujo <alexsaspbr@gmail.com>
2023-10-09 21:59:00 -0300

    initial commit

---
+++ src/main/java/tech/ada/pwiisantandercoders/PwiiSantanderCodersApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.pwiisantandercoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwiiSantanderCodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwiiSantanderCodersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ada/pwiisantandercoders/PwiiSantanderCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.pwiisantandercoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwiiSantanderCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
